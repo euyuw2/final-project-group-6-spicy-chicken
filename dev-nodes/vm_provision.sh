44a0ca76a6a90acf1f6fd5b71bd82b1af6559cf6
uttie <uttieg@gmail.com>
2020-02-18 12:56:59 -0800

    add docker-compose to vm

--- dev-nodes/vm_provision.sh
+++ dev-nodes/vm_provision.sh
@@ -29,3 +29,9 @@ sudo apt-get update
 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
+# add user to docker group
+sudo usermod -aG docker $USER
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
