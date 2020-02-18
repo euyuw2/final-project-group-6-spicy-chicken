df7b26fab9371f70469f87de63452c5478315b03
uttie <uttieg@gmail.com>
2020-02-18 11:02:15 -0800

    add script to provision vm

---
+++ dev-nodes/vm_provision.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#
+# This script is intended to provision a machine with all necessary things to
+# get subsequent software running.
+#
+# Typically used by a build system or Vagrant to provision the Base OS
+# necessities.
+#
+
+
+sudo apt-get update
+
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository -y\
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
