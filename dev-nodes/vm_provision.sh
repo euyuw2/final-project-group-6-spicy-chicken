b145fed06a6c59565caf8401b2adb2966ce578ce
uttie <uttieg@gmail.com>
2020-02-18 18:06:12 -0800

    add comments to provision file

--- dev-nodes/vm_provision.sh
+++ dev-nodes/vm_provision.sh
@@ -8,9 +8,8 @@
 # necessities.
 #
 
-
+# update lists and install basic stuff
 sudo apt-get update
-
 sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
@@ -18,15 +17,13 @@ sudo apt-get -y install \
     gnupg-agent \
     software-properties-common
 
+# Add docker to repo lists and install it.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 sudo add-apt-repository -y\
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
 sudo apt-get update
-
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # add user to docker group
